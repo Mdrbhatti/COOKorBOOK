ceeb2cc52af24099694ad539b98a9def22a2be31
Mdrbhatti <Mdrbhatti@gmail.com>
2017-06-15 15:24:48 +0200

    Update app module and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,23 +6,25 @@ import { AppComponent } from './app.component';
 import { CbFindFoodComponent } from './components/cb-find-food/cb-find-food.component';
 import { CbHeaderComponent } from './components/shared/cb-header/cb-header.component';
 import { FrontpageComponent } from './components/frontpage/frontpage.component';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
 
 const appRoutes: Routes = [
   { path: 'find-food', component: CbFindFoodComponent },
-  { path: '', component: FrontpageComponent, pathMatch: 'full'  }
+  { path: '', component: FrontpageComponent, pathMatch: 'full'  },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent }
 ];
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    CbFindFoodComponent,
     CbHeaderComponent,
+    CbFindFoodComponent,
     FrontpageComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
