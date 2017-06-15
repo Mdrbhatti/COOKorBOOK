c08aae3a1df224819331e564b42727e3221271ed
Shumail Mohyuddin <shumailmohyuddin@gmail.com>
2017-06-15 16:00:30 +0200

    refactor(find-food): structure & UI setup

--- src/app/components/cb-find-food/cb-find-food.component.ts
+++ src/app/components/cb-find-food/cb-find-food.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FoodItem } from './food'
 
 @Component({
   selector: 'app-cb-find-food',
@@ -6,8 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cb-find-food.component.scss']
 })
 export class CbFindFoodComponent implements OnInit {
+  foodItems: FoodItem[] = [];
 
-  constructor() { }
+  constructor() {
+    this.foodItems.push(new FoodItem('Speghaetti', 50, 'http://drop.ndtv.com/albums/COOKS/corngallery/creolespicedcornthumb_640x480.jpg'));
+    this.foodItems.push(new FoodItem('Cheesy Homemade Lasagna.', 40, 'http://assets.kraftfoods.com/recipe_images/opendeploy/Table-for-Two_Lasagna_640x428.jpg'));
+    this.foodItems.push(new FoodItem('Parmesan Cheese Pasta', 35, 'http://food.fnr.sndimg.com/content/dam/images/food/fullset/2011/2/4/1/RX-FNM_030111-Lighten-Up-012_s4x3.jpg.rend.hgtvcom.616.462.jpeg'));
+
+  }
 
   ngOnInit() {
   }
