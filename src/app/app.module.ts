0f221e20051d737db9c860b4aa083766c8fd8332
Øystein Hauan <oysteibh@stud.ntnu.no>
2017-06-15 15:06:27 +0200

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FrontpageComponent } from './frontpage/frontpage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+
+    FrontpageComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [FrontpageComponent]
 })
 export class AppModule { }
