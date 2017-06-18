2cd0428f075fb6316fd67f97e6ace1d4654a0f60
Shumail Mohyuddin <shumailmohyuddin@gmail.com>
2017-06-18 14:29:47 +0200

    feat(find-food): search by name for food items & mock json

--- src/app/components/cb-find-food/cb-find-food.component.ts
+++ src/app/components/cb-find-food/cb-find-food.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FoodItem } from './food'
+import { FoodItem } from './food';
+import { fooditemsMock } from './food-mock';
 
 @Component({
   selector: 'app-cb-find-food',
@@ -8,15 +9,33 @@ import { FoodItem } from './food'
 })
 export class CbFindFoodComponent implements OnInit {
   foodItems: FoodItem[] = [];
+  foodItemsToDisplay: FoodItem[] = [];
+  serachInput: string;
 
   constructor() {
-    this.foodItems.push(new FoodItem('Speghaetti', 50, 'http://drop.ndtv.com/albums/COOKS/corngallery/creolespicedcornthumb_640x480.jpg'));
-    this.foodItems.push(new FoodItem('Cheesy Homemade Lasagna.', 40, 'http://assets.kraftfoods.com/recipe_images/opendeploy/Table-for-Two_Lasagna_640x428.jpg'));
-    this.foodItems.push(new FoodItem('Parmesan Cheese Pasta', 35, 'http://food.fnr.sndimg.com/content/dam/images/food/fullset/2011/2/4/1/RX-FNM_030111-Lighten-Up-012_s4x3.jpg.rend.hgtvcom.616.462.jpeg'));
 
+    for (const fooditem of fooditemsMock) {
+      this.foodItems.push(new FoodItem( fooditem.name,
+                                        fooditem.description,
+                                        fooditem.sellerComments,
+                                        fooditem.pricePerPortion,
+                                        fooditem.bulkPricing,
+                                        fooditem.image,
+                                        fooditem.type,
+                                        fooditem.rating,
+                                        fooditem.addressStreet,
+                                        fooditem.addressPostalCode,
+                                        fooditem.addressCity
+                                      ));
+    }
+    this.foodItemsToDisplay = this.foodItems.slice();
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
+  search() {
+    this.foodItemsToDisplay = this.foodItems.slice().filter((el) =>
+      el.name.toLowerCase().indexOf(this.serachInput.toLowerCase()) > -1
+    )
+  }
 }
