b520122d970632808981eed604c1e2cf42f09e5d
Shumail Mohyuddin <shumailmohyuddin@gmail.com>
2017-07-16 16:29:59 +0200

    style(header): change color scheme to green

--- src/app/components/shared/cb-header/cb-header.component.ts
+++ src/app/components/shared/cb-header/cb-header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cb-header.component.scss']
 })
 export class CbHeaderComponent implements OnInit {
-
+  toggleOnMobile = false;
   constructor() { }
 
   ngOnInit() {
