63348d52452575497cd8a407126eca051f9ef935
Mdrbhatti <Mdrbhatti@gmail.com>
2017-07-18 05:42:50 +0200

    feat: add safeguard

--- src/app/components/cb-order-item/cb-order-item.component.ts
+++ src/app/components/cb-order-item/cb-order-item.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { BackendService } from '../../services/backend.service';
 
 @Component({
@@ -9,16 +9,33 @@ import { BackendService } from '../../services/backend.service';
 })
 export class CbOrderItemComponent implements OnInit, OnDestroy {
   id: string;
+  item: any = null;
   private sub: any;
 
-  constructor(private bcService: BackendService, private route: ActivatedRoute) { }
+  constructor(private bcService: BackendService, private route: ActivatedRoute,
+  private router: Router) { }
 
   ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
-       this.id = params['id'];
-       // assume some food id 
-       // Check validity of id
+      this.id = params['id'];
+      this.getItem();
+      // assume some food id 
+      // Check validity of id
     });
+  }
+
+  getItem() {
+    this.bcService.getPublishedItems(this.id).subscribe(
+      (res: any[]) => {
+        console.log("Item received for ordering: " + res.length);
+        console.log(res[0]);
+        this.item=res[0];
+      },
+      (error) => { console.log(error); 
+        setTimeout(() => { this.router.navigate(['/find-food']); }, 2000);
+        console.log("Failed while retrieving order item");
+    }
+    );
   }
 
   ngOnDestroy() {
