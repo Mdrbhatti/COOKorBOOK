431b165eabcad63cf8ce4cca15597329293ba3ae
Mdrbhatti <Mdrbhatti@gmail.com>
2017-07-19 22:37:13 +0200

    feat: update module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -34,7 +34,7 @@ const appRoutes: Routes = [
   { path: 'cook-food', component: CbPublishItemComponent, canActivate: [AuthGuard] },
   { path: 'order-food/:id', component: CbOrderItemComponent, canActivate: [AuthGuard] },
   { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] }, // TODO: Add ID here
-  // { path: '**', redirectTo: '', pathMatch: 'full' },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
 @NgModule({
